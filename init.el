b7b028b04276e9dabd483626071f3c1aea4d80c6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-05-29 12:07:41 +0900

    Adjust the font size

--- init.el
+++ init.el
@@ -110,7 +110,7 @@ values."
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Midway"
-                               :size 10.5
+                               :size 8.5
                                :weight normal
                                :width normal
                                :powerline-scale 1.2)
