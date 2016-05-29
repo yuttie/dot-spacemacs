6ad2ea5d77efe5eda0fe3e801f1a818b4ad36f54
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-05-29 09:40:04 +0900

    Escpae from everything by "tn"

--- init.el
+++ init.el
@@ -249,6 +249,8 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  ;; Escape
+  (setq-default evil-escape-key-sequence "tn")
   ;; Swap C-x and C-t
   (defun swap-C-x-and-C-t (frame)
     (with-selected-frame frame
