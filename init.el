3efb1a95cfc9c7de1102d44e9e9c2996f7fffdba
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2016-10-15 20:35:41 +0900

    Customize the face 'mmm-default-submode-face'

--- init.el
+++ init.el
@@ -528,3 +528,9 @@ real part."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mmm-default-submode-face ((t (:background "gray97")))))
