f4447639ce3ddc2502a105f206ae32f2360fe924
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-02-09 05:38:09 +0900

    Use my own hydrangea theme

--- layers/hydrangea-theme/packages.el
+++ layers/hydrangea-theme/packages.el
@@ -1,5 +1,6 @@
 (setq hydrangea-theme-packages
       '((hydrangea-theme :location (recipe
                                     :fetcher github
-                                    :repo "yuttie/hydrangea-theme"))))
+                                    :repo "yuttie/hydrangea-theme"
+                                    :branch "v4"))))
 (defun hydrangea-theme/init-hydrangea-theme ())
